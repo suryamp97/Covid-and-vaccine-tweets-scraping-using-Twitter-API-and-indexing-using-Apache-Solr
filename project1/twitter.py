9a6db82cabdf9bae0526d3001a5e7feb15efc883
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-17 14:38:27 -0700

    Update twitter.py

--- project1/twitter.py
+++ project1/twitter.py
@@ -27,7 +27,7 @@ class Twitter:
         '''
         tweets = []
         c=0
-        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=5).items(5):                    
+        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=2500).items(2500):                    
             if tweet.retweeted :
                 c=c+1
                 if c<225:
