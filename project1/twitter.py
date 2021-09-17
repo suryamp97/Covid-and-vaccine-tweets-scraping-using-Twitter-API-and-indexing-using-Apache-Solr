13f2792fc453b26f544fe213e709792fb16133e1
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-16 18:21:50 -0700

    Update twitter.py

--- project1/twitter.py
+++ project1/twitter.py
@@ -26,11 +26,15 @@ class Twitter:
         :return: List
         '''
         tweets = []
-        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=1500).items(1500):
-            c=0;
-            txt_ = tweet._json["text"]
-            if tw_json["text"]
-            tweets.append(tweet._json)
+        c=0
+        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=1500).items(1500):                    
+            if tweet.retweeted :
+                c=c+1
+                if c<225:
+                    tweets.append(tweet._json)
+            else 
+                tweets.append(tweet._json)
+
         #print(tweets)
         return tweets
 
