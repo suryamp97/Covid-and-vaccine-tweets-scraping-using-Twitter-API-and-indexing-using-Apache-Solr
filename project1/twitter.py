e39f22e352c413740b62e4997c54aee118eaeead
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-22 13:20:38 -0700

    Update twitter.py

--- project1/twitter.py
+++ project1/twitter.py
@@ -71,7 +71,7 @@ class Twitter:
         tweets = []
         c=0
         poi_twids = []
-        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=3200).items(3200):    
+        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=1500).items(3200):    
             tj=tweet._json
             txt = tj["text"]
             if any(k in txt for k in keys):
