ce24561d8016e6e7b5d61be088931b1827146c14
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-23 17:28:31 -0400

    Update twitter.py

--- project1/twitter.py
+++ project1/twitter.py
@@ -64,30 +64,32 @@ class Twitter:
         For more info: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/guides/working-with-timelines
         :return: List
         '''
-        keys = []
-        for i in range(25):
-            keys.append(keywords[i]['name'])
+#         keys = []
+#         for i in range(25):
+#             keys.append(keywords[i]['name'])
 
         tweets = []
         c=0
-        poi_twids = []
+        twids = [1435742326639538176,1435743948186718217,1435740397221957634,1435740270382002184,1435754550477168641,1436091066957148163,1435741310799994883,1435770947018051590,1435754919995248642,1435741059217178629]
 
-
-        for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=1500).items(1500):    
-            tj=tweet._json
-            txt = tj["text"]
-            if any(k in txt for k in keys):
-                if txt.startswith('RT @'):
-                    c=c+1
-                    if c<2:
-                        poi_twids.append(tj['id'])
-                else :
-                    poi_twids.append(tj['id'])
+        
+        for idd in twids:
+            for tweet in tweepy.Cursor(self.api.search,q='s',max_id=idd,since_id=idd, count=5).items(5):
+                tweets.append(tweet)
+#         for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, count=1500).items(1500):    
+#             tj=tweet._json
+#             txt = tj["text"]
+#             if any(k in txt for k in keys):
+#                 if txt.startswith('RT @'):
+#                     c=c+1
+#                     if c<2:
+#                         poi_twids.append(tj['id'])
+#                 else :
+#                     poi_twids.append(tj['id'])
             
         
-        print(poi_twids)
         
-        print("covid tweets ",screen_name,len(poi_twids))
+        #print("covid tweets ",screen_name,len(poi_twids))
         
 #         for i in range(len(poi_twids)):
 #             idd=poi_twids[i]
